88d16517b1c40d4501f02ff550858291421de96e
Santiago <vargassantiago97@gmail.com>
2025-05-22 08:41:37 -0300

    v1

--- src/assets/configuraciones.js
+++ src/assets/configuraciones.js
@@ -1,28 +1,20 @@
 var vars = {
-    AUTH_URI: 'http://localhost:3001',
-    API_URI: 'http://localhost:3001',
-    PDF_URI: 'http://localhost:3001',
-    XLSX_URI: 'http://localhost:3001',
+    AUTH_URI: 'http://26.219.132.16:3001',
+    API_URI: 'http://26.219.132.16:3001',
+    PDF_URI: 'http://26.219.132.16:3001',
+    XLSX_URI: 'http://26.219.132.16:3001',
     PADRON_URI: 'http://46.202.147.144/api/afip/api',
 
     modelosRemitos: [{ //ACTIVO EL PRIMERO
         id: 1,
         alias: "moliendas",
         descripcion: "Moliendas"
-    },{
-        id: 2,
-        alias: "norsemillas",
-        descripcion: "NorSemillas"
     }],
 
     puntosVenta: [{ //ACTIVO EL PRIMERO
         id: 1,
         punto: 1,
         descripcion: "1"
-    },{
-        id: 2,
-        punto: 2,
-        descripcion: "2"
     }],
 
     //depositoSeleccionado: '46968435-013e-4fef-bfd2-00d60f74a204'
